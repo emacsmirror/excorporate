09f998e8a4ff653a8cad1bf5d40411e13157ab1f
Thomas Fitzsimmons <fitzsim@fitzsim.org>
2016-03-16 11:42:34 -0400

    packages/excorporate: Bump version to 0.7.2

--- excorporate.el
+++ excorporate.el
@@ -5,7 +5,7 @@
 ;; Author: Thomas Fitzsimmons <fitzsim@fitzsim.org>
 ;; Maintainer: Thomas Fitzsimmons <fitzsim@fitzsim.org>
 ;; Created: 2014-09-19
-;; Version: 0.7.1
+;; Version: 0.7.2
 ;; Keywords: calendar
 ;; Homepage: https://www.fitzsim.org/blog/
 ;; Package-Requires: ((emacs "24.1") (fsm "0.2") (soap-client "3.1.1") (url-http-ntlm "2.0.2"))
