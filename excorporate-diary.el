ebf91e490ef16cbca1429dae9dac79e96ad3a819
Thomas Fitzsimmons <fitzsim@fitzsim.org>
2019-06-17 19:03:47 -0400

    excorporate-diary: Update warning message
    * packages/excorporate/excorporate-diary.el
    (excorporate-diary-enable): Update diary-fancy-display warning
    message.

--- excorporate-diary.el
+++ excorporate-diary.el
@@ -275,7 +275,7 @@ ARGUMENTS are the arguments to `diary-view-entries'."
   (unless (eq diary-display-function 'diary-fancy-display)
     (warn (format
 	   (concat "Excorporate diary support needs diary-fancy-display"
-		   " but diary-display-function is currently %S; overriding")
+		   " but diary-display-function is currently %S")
 	   diary-display-function)))
   (appt-activate 1)
   (message "Excorporate diary support enabled."))
