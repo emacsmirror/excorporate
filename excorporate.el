29bef79b3b9620498a8c4d8a69bfde0c345f4d80
Thomas Fitzsimmons <fitzsim@fitzsim.org>
2020-09-30 22:33:18 -0400

    Excorporate: Bump soap-client required version to 3.2.0
    * packages/excorporate/excorporate.el: Bump soap-client required
    version to 3.2.0.

--- excorporate.el
+++ excorporate.el
@@ -8,7 +8,7 @@
 ;; Version: 0.8.3
 ;; Keywords: calendar
 ;; Homepage: https://www.fitzsim.org/blog/
-;; Package-Requires: ((emacs "24.1") (fsm "0.2.1") (soap-client "3.1.5") (url-http-ntlm "2.0.4") (nadvice "0.3"))
+;; Package-Requires: ((emacs "24.1") (fsm "0.2.1") (soap-client "3.2.0") (url-http-ntlm "2.0.4") (nadvice "0.3"))
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
